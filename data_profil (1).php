2057feaffc37750a14b99eeda11e809aeebc7cd5
ahlussodikin <58922959+ahlussodikin@users.noreply.github.com>
2021-07-10 23:30:02 -0700

    Add files via upload

---
+++ data_profil (1).php
@@ -0,0 +1,35 @@
+<!-- Main content -->
+<section class="content">
+
+
+	<!-- /.box-header -->
+	<div class="box box-solid box-info">
+		<div class="box-header with-border">
+			<h3 class="box-title" style="font-weight: bold; color: black;">PROFILE SEKOLAH</h3>
+
+		</div>
+		<div class="table-responsive">
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+
+						<th style="text-align: center;">Nama Sekolah</th>
+						<th style="text-align: center;">Alamat</th>
+						<th style="text-align: center;">Akreditasi</th>
+						<th style="text-align: center;">NPSN</th>
+					</tr>
+				</thead>
+				<tbody>
+
+
+					<th style="text-align: center;"> SD NEGERI 1 SEMBUNG</th>
+					<th style="text-align: center;">Sembung, Kec. Narmada, Kab. Lombok Barat, Prov. Nusa Tenggara Barat, 83371</th>
+					<th style="text-align: center;">A</th>
+					<th style="text-align: center;">50200772</th>
+				</tbody>
+
+			</table>
+		</div>
+	</div>
+	</div>
+</section>
